5a40d9543d79e8ba7b19bd5950b34acd11a8e356
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 00:13:47 -0600

    Agregue comentario para probar los merges de master y main.

--- src/java/clientes/ClienteLibros.java
+++ src/java/clientes/ClienteLibros.java
@@ -1,4 +1,5 @@
 /* Giovanni Olmos Salmones - 2172002785
+ * Gabriel Hurtado Aviles - 2172000781
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
