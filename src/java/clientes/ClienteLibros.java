91466f32dcb886b859ea5f3212d98ef3bac6706e
Giovanni Olmos <xidotwo@gmail.com>
2022-01-25 00:10:14 -0600

    Realize modificacion de la imagen principal del formulario de index.html

--- src/java/clientes/ClienteLibros.java
+++ src/java/clientes/ClienteLibros.java
@@ -1,4 +1,4 @@
-/* Giovanni Olmos Salmones 
+/* Giovanni Olmos Salmones - 2172002785
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
